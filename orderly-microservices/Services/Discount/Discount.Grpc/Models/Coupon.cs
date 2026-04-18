305b1b6472ea6cbfedad1de460491b751ce8ed7b
Omar Martínez <omartinez@bakerrobotics.com>
2026-04-18 11:25:59 -0600

    Add RedeemAmount new property

--- orderly-microservices/Services/Discount/Discount.Grpc/Models/Coupon.cs
+++ orderly-microservices/Services/Discount/Discount.Grpc/Models/Coupon.cs
@@ -9,6 +9,7 @@ public class Coupon : AuditableEntity<int>
     public required string Code { get; set; }
     public required string Description { get; set; }
     public required decimal Amount { get; set; }
+    public int RedeemAmount { get; set; } = 0;
     public int? MaxRedeemAmount { get; set; }
     public Instant? ExpirationDate { get; set; }
 }

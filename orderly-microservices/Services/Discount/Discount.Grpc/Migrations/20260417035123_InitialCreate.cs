53fff51ec3971b052d2b3cb37bdb4ee528d0b5c3
Omar Martínez <omartinez@bakerrobotics.com>
2026-04-18 11:33:43 -0600

    Adding discount migrations

---
+++ orderly-microservices/Services/Discount/Discount.Grpc/Migrations/20260417035123_InitialCreate.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coupons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    RestaurantId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Code = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Amount = table.Column<decimal>(type: "TEXT", nullable: false),
+                    MaxRedeemAmount = table.Column<int>(type: "INTEGER", nullable: true),
+                    ExpirationDate = table.Column<long>(type: "INTEGER", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: false),
+                    CreatedOn = table.Column<long>(type: "INTEGER", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "TEXT", nullable: false),
+                    LastModifiedOn = table.Column<long>(type: "INTEGER", nullable: true),
+                    IsActive = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coupons", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Coupons",
+                columns: new[] { "Id", "Amount", "Code", "CreatedBy", "CreatedOn", "Description", "ExpirationDate", "IsActive", "LastModifiedBy", "LastModifiedOn", "MaxRedeemAmount", "RestaurantId" },
+                values: new object[,]
+                {
+                    { 1, 10m, "DISCOUNT10", "System", 17040672000000000L, "10% off your order", 17356895990000000L, true, "System", null, 100, new Guid("11111111-1111-1111-1111-111111111111") },
+                    { 2, 20m, "DISCOUNT20", "System", 17040672000000000L, "20% off your order", 17356895990000000L, true, "System", null, 200, new Guid("22222222-2222-2222-2222-222222222222") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Coupons");
+        }
+    }
+}
---
+++ orderly-microservices/Services/Discount/Discount.Grpc/Migrations/20260418172446_IncludeAmountRedeemed.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Discount.Grpc.Migrations
+{
+    /// <inheritdoc />
+    public partial class IncludeAmountRedeemed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RedeemAmount",
+                table: "Coupons",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "RedeemAmount",
+                value: 0);
+
+            migrationBuilder.UpdateData(
+                table: "Coupons",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "RedeemAmount",
+                value: 10);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RedeemAmount",
+                table: "Coupons");
+        }
+    }
+}

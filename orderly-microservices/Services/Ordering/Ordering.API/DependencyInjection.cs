488bed13feb26981b69978abc1973b1fa06e8505
Omar Martínez <omartinez@bakerrobotics.com>
2026-04-25 10:58:36 -0600

    initialize Ordering microservice solution structure and project dependencies

---
+++ orderly-microservices/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿namespace Ordering.API;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    {
+        // services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
+        return services;
+    }
+
+    public static WebApplicationBuilder UseApiServices(this WebApplicationBuilder app)
+    {
+        // app.MapCarter();
+        return app;
+    }
+}

83a4ad68271f5cd4564663bdd5a632950093e8dc
Omar Martínez <omartinez@bakerrobotics.com>
2026-06-06 17:37:40 -0600

    added the standard shipping/billing address and payment information properties for the checkout event

--- BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
+++ BuildingBlocks.Messaging/Events/BasketCheckoutEvent.cs
@@ -7,6 +7,22 @@ public record BasketCheckoutEvent : IntegrationEvent
     public List<BasketCheckoutItem> Items { get; init; } = [];
     public List<string> AppliedDiscounts { get; init; } = [];
     public decimal TotalAmount { get; init; }
+
+    // Shipping and Billing Address
+    public string FirstName { get; init; } = default!;
+    public string LastName { get; init; } = default!;
+    public string EmailAddress { get; init; } = default!;
+    public string AddressLine { get; init; } = default!;
+    public string Country { get; init; } = default!;
+    public string State { get; init; } = default!;
+    public string ZipCode { get; init; } = default!;
+
+    // Payment
+    public string CardName { get; init; } = default!;
+    public string CardNumber { get; init; } = default!;
+    public string Expiration { get; init; } = default!;
+    public string CVV { get; init; } = default!;
+    public int PaymentMethod { get; init; }
 }
 
 public class BasketCheckoutItem
